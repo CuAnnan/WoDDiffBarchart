469b9c4eab75b988fb6679b93f93176ac9ef8ea3
Wing <eamonn.kearns@so-4pt.net>
2022-06-24 02:02:51 +0100

    Initial commit

---
+++ DicePool.js
@@ -0,0 +1,43 @@
+class DicePool
+{
+    constructor(data)
+    {
+        this.size = data.size;
+        this.spec = data.spec?data.spec:false;
+        this.diff = data.diff?data.diff:6;
+        this.willpower = data.willpower?data.willpower:false;
+    }
+
+    roll()
+    {
+        let diceRolled = [];
+        let successesRolled = 0;
+        let onesRolled = 0;
+        while(diceRolled.length < this.size)
+        {
+            let dieRolled = 1 + Math.floor(Math.random() * 10);
+            diceRolled.push(dieRolled);
+            if(dieRolled == 1)
+            {
+                onesRolled += 1;
+            }
+            else if(dieRolled >= this.diff)
+            {
+                successesRolled ++;
+                if(dieRolled == 10 && this.spec)
+                {
+                    successesRolled ++;
+                }
+            }
+        }
+
+        let botch = successesRolled == 0 && onesRolled >=1 && !this.willpower;
+        let successes = successesRolled - onesRolled + (this.willpower?1:0);
+
+        return {
+            diceRolled:diceRolled,
+            successes:successes,
+            botch:botch
+        }
+    }
+}
---
+++ driver.js
@@ -0,0 +1,33 @@
+let DicePool = require('./DicePool');
+
+let pool = new DicePool({diff:8, spec:true, size:8, willpower:true});
+
+let timesSucceeded = 0, timesFailed = 0;
+
+for(let i = 1; i < 10001; i++)
+{
+    let successes = 0;
+    let botched = false;
+    for(let j = 0; j < 4; j++)
+    {
+        let roll = pool.roll();
+        if(roll.successes >= 0) {
+            successes += roll.successes;
+        }
+        if(roll.botch)
+        {
+            botched = true;
+        }
+    }
+
+    if(!botched && successes >= 16)
+    {
+        timesSucceeded ++;
+    }
+    else
+    {
+        timesFailed ++;
+    }
+
+    console.log(`Success rate of ${timesSucceeded}/${i} for ${timesSucceeded/i}`)
+}
